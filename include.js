493b3738cf8fb89ab19085106378546025e6ec6b
Laise Costa <laise_costa@hotmail.com>
2025-05-11 11:51:04 -0300

    Atualização js para GitHub Pages

--- include.js
+++ include.js
@@ -2,15 +2,21 @@ window.loadHTML = async function(id, fileName) {
   const targetElement = document.getElementById(id);
   if (!targetElement) return;
 
-  // Determina o caminho correto para o arquivo
-  const isInPages = window.location.pathname.includes('/pages/');
-  const fullPath = isInPages ? `../includes/${fileName}` : `/includes/${fileName}`;
+  // Caminho base
+  const isGitHubPages = window.location.hostname.includes('github.io'); 
+  let includePath = '';
+
+  // Live Server
+  if (!isGitHubPages) {
+    includePath = `../includes/${fileName}`; // Caminho relativo no Live Server
+  } else {
+    // GitHub Pages
+    includePath = `https://icei-puc-minas-pmv-si.github.io/pmv-si-2025-1-pe1-t2-organizapro/includes/${fileName}`;
+  }
 
   try {
-    const response = await fetch(fullPath);
-    if (!response.ok) {
-      throw new Error(`Erro ao carregar "${fileName}": ${response.statusText}`);
-    }
+    const response = await fetch(includePath);
+    if (!response.ok) throw new Error(`Erro ao carregar "${fileName}": ${response.statusText}`);
     const html = await response.text();
     targetElement.innerHTML = html;
   } catch (error) {
