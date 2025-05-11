78b73edb1cf76b9114b9cb05534c45a18eefc03b
Laise Costa <laise_costa@hotmail.com>
2025-05-11 00:34:16 -0300

    Corrige include.js

--- js/include.js
+++ js/include.js
@@ -1,14 +1,21 @@
-function loadHTML(id, file) {
+window.loadHTML = async function(id, file) {
   const targetElement = document.getElementById(id);
-  if (!targetElement) return Promise.resolve(); 
+  if (!targetElement) return;
 
-  return fetch(file)
-    .then(response => {
-      if (!response.ok) throw new Error(`Erro ao carregar "${file}": ${response.statusText}`);
-      return response.text();
-    })
-    .then(html => {
-      targetElement.innerHTML = html;
-    })
-    .catch(error => console.error(`Erro ao carregar "${file}":`, error));
-}
+  try {
+    const response = await fetch(file);
+    if (!response.ok) throw new Error(`Erro ao carregar "${file}": ${response.statusText}`);
+    const html = await response.text();
+    targetElement.innerHTML = html;
+  } catch (error) {
+    console.error(`Erro ao carregar "${file}":`, error);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadHTML('header', '../includes/header.html');
+  loadHTML('sidebar', '../includes/sidebar.html');
+  loadHTML('favorites', '../includes/favorites.html');
+  loadHTML('recent-upd', '../includes/recent-upd.html');
+  loadHTML('tasks', '../includes/tasks.html');
+});
